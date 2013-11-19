14069e37b1bfc7bdc8886a18e808ca81b4bd6442
pejot <pejot@pejot-linux-station.(none)>
2013-11-19 17:35:53 +0100

    Author text box cleaned once add author button is clicked.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 $(function() {
+  var new_author_div = $("#author_group").clone();
   $("#add_author_button").click(function(){
-    $("#author_group").clone().appendTo( "#authors_list");
+    new_author_div.appendTo( "#authors_list");
+    new_author_div = new_author_div.clone();
   });
 });
